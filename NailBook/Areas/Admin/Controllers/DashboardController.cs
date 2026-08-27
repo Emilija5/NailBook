c3de47623a28131b592428ca55fcd5817d82c927
Emilija5 <emilijaantovska.2005@gmail.com>
2026-08-27 14:30:06 +0200

    Show pending appointments on admin dashboard

--- NailBook/Areas/Admin/Controllers/DashboardController.cs
+++ NailBook/Areas/Admin/Controllers/DashboardController.cs
@@ -1,6 +1,8 @@
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using NailBook.Data;
 using NailBook.Models;
+using NailBook.Models.Enums;
 
 namespace NailBook.Areas.Admin.Controllers;
 
@@ -8,8 +10,20 @@ namespace NailBook.Areas.Admin.Controllers;
 [Authorize(Roles = RoleNames.Admin)]
 public class DashboardController : Controller
 {
+    private readonly ApplicationDbContext _context;
+
+    public DashboardController(ApplicationDbContext context)
+    {
+        _context = context;
+    }
+
     public IActionResult Index()
     {
+        var pendingAppointmentsCount = _context.Appointments.Count(
+            appointment => appointment.Status == AppointmentStatus.Pending);
+
+        ViewBag.PendingAppointmentsCount = pendingAppointmentsCount;
+
         return View();
     }
 }
