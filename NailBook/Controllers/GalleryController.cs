7389343a47410ee101b2240ac59005c1d327b9ad
Emilija5 <emilijaantovska.2005@gmail.com>
2026-08-29 17:31:58 +0200

    Add gallery style filter

--- NailBook/Controllers/GalleryController.cs
+++ NailBook/Controllers/GalleryController.cs
@@ -1,5 +1,8 @@
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using NailBook.Data;
+using NailBook.Models;
+using NailBook.Models.Enums;
 
 namespace NailBook.Controllers;
 
@@ -12,12 +15,25 @@ public class GalleryController : Controller
         _context = context;
     }
 
-    public IActionResult Index()
+    public IActionResult Index(NailDesignStyle? style)
     {
-        var nailDesigns = _context.NailDesigns
-            .OrderByDescending(design => design.CreatedAt)
-            .ToList();
+        IQueryable<NailDesign> nailDesigns = _context.NailDesigns;
 
-        return View(nailDesigns);
+        if (style.HasValue)
+        {
+            nailDesigns = nailDesigns.Where(design => design.Style == style.Value);
+        }
+
+        ViewBag.Styles = Enum.GetValues<NailDesignStyle>()
+            .Select(styleOption => new SelectListItem
+            {
+                Value = styleOption.ToString(),
+                Text = styleOption.ToString(),
+                Selected = style == styleOption
+            });
+
+        return View(nailDesigns
+            .OrderByDescending(design => design.CreatedAt)
+            .ToList());
     }
 }
